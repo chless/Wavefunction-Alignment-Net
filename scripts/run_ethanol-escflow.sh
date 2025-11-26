85733f32d66f897a97b5d9f0e75bf842335afa18
chless <chelssling@gmail.com>
2025-11-26 17:15:11 +0900

    add training bashes

---
+++ scripts/run_ethanol-escflow.sh
@@ -0,0 +1,19 @@
+python pipelines/train.py \
+--config-name=config.yaml \
+job_id="md17-ethanol" \
+data_name=escflow-ethanol \
+dataset_path="/nas/qhflow-mlff/dataset/ethanol_shard/processed/lmdbs" \
+devices=[2] \
+wandb.wandb_api_key=$WANDB_API_KEY \
+lr=0.0005 \
+enable_hami=True \
+hami_weight=1  \
+batch_size=32 \
+schedule=polynomial \
+schedule.lr_warmup_steps=1000 \
+max_steps=200000 \
+train_ratio=0.8 \
+val_ratio=0.1 \
+test_ratio=0.1 \
+gradient_clip_val=5.0 \
+dataset_size=-1
