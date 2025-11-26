81cb336aad5b0d05e0ca24071eaeeb1194c11eb4
chless <chelssling@gmail.com>
2025-11-26 16:28:00 +0900

    add bash for qh9 training

--- scripts/run_qh9-escflow.sh
+++ scripts/run_qh9-escflow.sh
@@ -1,8 +1,23 @@
-python pipelines/train.py --config-name=config.yaml wandb.open=True job_id=QH9 \
-model_backbone=QHNet_backbone_No lr=0.0005 enable_hami=True \
-hami_weight=1  batch_size=32 inference_batch_size=256 devices=[7] schedule=polynomial schedule.lr_warmup_steps=1000 \
-max_steps=300000 used_cache=True \
-train_ratio=0.9 val_ratio=0.06 test_ratio=0.04  gradient_clip_val=5.0 dataset_size=100000
+python pipelines/train.py --config-name=config.yaml \
+job_id=QH9 \
+data_name=escflow_qh9stable \
+dataset_path="/nas/qhflow-mlff/dataset/QH9Stable_shard/processed/lmdbs" \
+model_backbone=QHNet_backbone \
+wandb.wandb_api_key=$WANDB_API_KEY \
+lr=0.0005 \
+enable_hami=True \
+hami_weight=1 \
+devices=[4,5,6,7] \
+batch_size=32 \
+inference_batch_size=224 \
+schedule=polynomial \
+schedule.lr_warmup_steps=1000 \
+max_steps=300000 \
+train_ratio=0.9 \
+val_ratio=0.06 \
+test_ratio=0.04  \
+gradient_clip_val=5.0 \
+dataset_size=100000
 
 
 # python pipelines/train.py --config-name=config.yaml wandb.open=True wandb.wandb_group="QH9" job_id=QHNet_SO2 wandb.wandb_api_key=6f1080f993d5d7ad6103e69ef57dd9291f1bf366 \
