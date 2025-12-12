fbbd5a144442d6a7972d5a7ba7189439ea384f75
chless <chelssling@gmail.com>
2025-12-12 13:39:53 +0900

    feat: update pyscf calculation parameters, change default xc functional, and refine ground truth data processing.

--- md17_evaluation_custom.py
+++ md17_evaluation_custom.py
@@ -4,7 +4,7 @@ import torch
 from pyscf import gto, scf, dft
 import time
 from escflow_eval_utils import init_pyscf_mf, calc_dm0_from_ham, matrix_transform_single
-from escflow_eval_utils import BOHR2ANG
+from escflow_eval_utils import BOHR2ANG, HA2meV, HA_BOHR_2_meV_ANG
 import numpy as np
 from multiprocessing import Pool
 from tqdm import tqdm
@@ -15,7 +15,7 @@ import json
 warnings.filterwarnings('ignore', category=FutureWarning)
 
 def process_single_molecule(pred_file_path, gt_file_path,
-    unit="ang", xc="pbe0", basis="def2svp", debug=False
+    unit="ang", xc="pbe", basis="def2svp", debug=False
 ):
     dir_path = os.path.dirname(pred_file_path)
     calc_path = pred_file_path.replace("pred_", "calc_")
@@ -30,171 +30,192 @@ def process_single_molecule(pred_file_path, gt_file_path,
 
     calc_mf = init_pyscf_mf(atoms, pos, unit=unit, xc=xc, basis=basis)
     grad_frame = calc_mf.nuc_grad_method()
-    NEW_CALC = False
-    try:
-        # Check if calculated data exists
-        if os.path.exists(calc_path):
-            calc_data = torch.load(calc_path)
-            calc_energy = calc_data["calc_energy"]
-            calc_forces = calc_data["calc_forces"]
-            calc_mo_energy = calc_data["calc_mo_energy"]
-            calc_mo_coeff = calc_data["calc_mo_coeff"]
-            calc_ham = calc_data["hamiltonian"]
-            calc_overlap = calc_data["overlap"]
-            
-        else:
-            calc_data = gt_data.copy()  # Use copy to avoid modifying original
-            start_time = time.time()
-            calc_mf.kernel()
-            calc_data["calc_time"] = time.time() - start_time
-            calc_data["hamiltonian"] = torch.tensor(calc_mf.get_fock(dm=calc_mf.make_rdm1()), dtype=torch.float64)
-            calc_data["overlap"] = torch.tensor(calc_mf.get_ovlp(), dtype=torch.float64)
-            calc_data["density_matrix"] = torch.tensor(calc_mf.make_rdm1(), dtype=torch.float64)
-            calc_data["method"] = "RKS"
-            calc_data["xc"] = xc
-            calc_data["basis"] = basis
-            calc_data["scf_cycles"] = calc_mf.cycles
-            calc_data["forces"] = torch.tensor(-grad_frame.kernel(), dtype=torch.float64)
-
-            calc_overlap = calc_data["overlap"].unsqueeze(0) # (gt_overlap - calc_overlap) has float32 precision error (1e^-7)
-            calc_ham = calc_data["hamiltonian"].unsqueeze(0)
-            
-            calc_density, calc_res = calc_dm0_from_ham(atoms, calc_overlap, calc_ham, transform=False)
-            calc_energy = calc_mf.energy_tot(calc_density)
-            calc_data["calc_energy"] = calc_energy
-
-            calc_mo_energy = calc_res["orbital_energies"].squeeze().numpy()
-            calc_mo_coeff = calc_res["orbital_coefficients"].squeeze().numpy()
-            calc_data["calc_mo_energy"] = calc_mo_energy
-            calc_data["calc_mo_coeff"] = calc_mo_coeff
-
-            mo_occ = calc_mf.get_occ(calc_mo_energy, calc_mo_coeff)
-            calc_data["mo_occ"] = mo_occ
-            calc_forces = -grad_frame.kernel(mo_energy=calc_mo_energy, mo_coeff=calc_mo_coeff, mo_occ=mo_occ)
-            calc_data["calc_forces"] = calc_forces
-
-            # save calc_data
-            if not debug:
-                torch.save(calc_data, calc_path)
-                NEW_CALC = True
+    calc_mf.conv_tol = 1e-7
+    calc_mf.grids.level = 3
+    calc_mf.grids.prune = None
+    calc_mf.small_rho_cutoff = 1e-12
+    DO_NEW_CALC = True
+    #try:
+    # Check if calculated data exists
+    if os.path.exists(calc_path) and not DO_NEW_CALC:
+        calc_data = torch.load(calc_path)
+        calc_energy = calc_data["calc_energy"]
+        calc_forces = calc_data["calc_forces"]
+        calc_mo_energy = calc_data["calc_mo_energy"]
+        calc_mo_coeff = calc_data["calc_mo_coeff"]
+        calc_ham = calc_data["hamiltonian"]
+        calc_overlap = calc_data["overlap"]
         
-        if "remove_init" not in gt_data.keys():
-            remove_init = True
-            gt_data["remove_init"] = remove_init
-            pred_data["remove_init"] = remove_init
-        else:
-            remove_init = gt_data["remove_init"]
+    else:
+        calc_data = gt_data.copy()  # Use copy to avoid modifying original
+        start_time = time.time()
+        calc_mf.kernel()
+        calc_data["calc_time"] = time.time() - start_time
+        calc_data["hamiltonian"] = torch.tensor(calc_mf.get_fock(dm=calc_mf.make_rdm1()), dtype=torch.float64)
+        calc_data["overlap"] = torch.tensor(calc_mf.get_ovlp(), dtype=torch.float64)
+        calc_data["density_matrix"] = torch.tensor(calc_mf.make_rdm1(), dtype=torch.float64)
+        calc_data["method"] = "RKS"
+        calc_data["xc"] = xc
+        calc_data["basis"] = basis
+        calc_data["scf_cycles"] = calc_mf.cycles
+        calc_data["forces"] = torch.tensor(-grad_frame.kernel(), dtype=torch.float64)
 
-        if "calc_force" in pred_data and not NEW_CALC:
-            pred_energy = pred_data["calc_energy"]
-            pred_forces = pred_data["calc_forces"]
-            pred_mo_energy = pred_data["calc_mo_energy"]
-            pred_mo_coeff = pred_data["calc_mo_coeff"]
-        else:
-            pred_hamiltonian = pred_data["pred_hamiltonian"] + remove_init * gt_data["init_ham"].squeeze(0)
+        calc_overlap = calc_data["overlap"].unsqueeze(0) # (gt_overlap - calc_overlap) has float32 precision error (1e^-7)
+        calc_ham = calc_data["hamiltonian"].unsqueeze(0)
+        
+        calc_density, calc_res = calc_dm0_from_ham(atoms, calc_overlap, calc_ham, transform=False)
+        calc_energy = calc_mf.energy_tot(calc_density)
+        calc_data["calc_energy"] = calc_energy
 
-            calc_overlap = calc_data["overlap"].unsqueeze(0) # (gt_overlap - calc_overlap) has float32 precision error (1e^-7)
-            pred_ham = matrix_transform_single(pred_hamiltonian.unsqueeze(0), atoms, convention="back2pyscf")
-            
-            pred_density, pred_res = calc_dm0_from_ham(atoms, calc_overlap, pred_ham, transform=False)
-            pred_energy = calc_mf.energy_tot(pred_density)
-            pred_data["calc_energy"] = pred_energy
-            
-            pred_mo_energy = pred_res["orbital_energies"].squeeze().numpy()
-            pred_mo_coeff = pred_res["orbital_coefficients"].squeeze().numpy()
-            pred_data["calc_mo_energy"] = pred_mo_energy
-            pred_data["calc_mo_coeff"] = pred_mo_coeff
-            
-            mo_occ = calc_mf.get_occ(pred_mo_energy, pred_mo_coeff)
-            pred_data["mo_occ"] = mo_occ
-            pred_forces = -grad_frame.kernel(mo_energy=pred_mo_energy, mo_coeff=-pred_mo_coeff, mo_occ=mo_occ)
-            pred_data["calc_forces"] = pred_forces
+        calc_mo_energy = calc_res["orbital_energies"].squeeze().numpy()
+        calc_mo_coeff = calc_res["orbital_coefficients"].squeeze().numpy()
+        calc_data["calc_mo_energy"] = calc_mo_energy
+        calc_data["calc_mo_coeff"] = calc_mo_coeff
 
-            # save pred_data
+        mo_occ = calc_mf.get_occ(calc_mo_energy, calc_mo_coeff)
+        calc_data["mo_occ"] = mo_occ
+        calc_forces = -grad_frame.kernel(mo_energy=calc_mo_energy, mo_coeff=calc_mo_coeff, mo_occ=mo_occ)
+        calc_data["calc_forces"] = calc_forces
+
+        # save calc_data
+        if not debug:
+            torch.save(calc_data, calc_path)
+    
+    if "remove_init" not in gt_data.keys():
+        remove_init = True
+        gt_data["remove_init"] = remove_init
+        pred_data["remove_init"] = remove_init
+    else:
+        remove_init = gt_data["remove_init"]
+
+    if "calc_force" in pred_data and "calc_force" in gt_data and not DO_NEW_CALC:
+        pred_energy = pred_data["calc_energy"]
+        pred_forces = pred_data["calc_forces"]
+        pred_mo_energy = pred_data["calc_mo_energy"]
+        pred_mo_coeff = pred_data["calc_mo_coeff"]
+
+        gt_energy = gt_data["calc_energy"]
+        gt_forces = gt_data["calc_forces"]
+        gt_mo_energy = gt_data["calc_mo_energy"]
+        gt_mo_coeff = gt_data["calc_mo_coeff"]
+    else:
+        gt_overlap = gt_data["overlap"]
+        gt_overlap = torch.from_numpy(gt_overlap)
+        gt_overlap = matrix_transform_single(gt_overlap, atoms, convention="back2pyscf")
+
+        pred_hamiltonian = pred_data["pred_hamiltonian"] + remove_init * gt_data["init_ham"].squeeze(0)
+        pred_ham = matrix_transform_single(pred_hamiltonian.unsqueeze(0), atoms, convention="back2pyscf")
+        
+        gt_hamiltonian = gt_data["hamiltonian"] + remove_init * gt_data["init_ham"].squeeze(0)
+        gt_ham = matrix_transform_single(gt_hamiltonian.unsqueeze(0), atoms, convention="back2pyscf")
+        
+        pred_density, pred_res = calc_dm0_from_ham(
+            atoms=atoms, 
+            overlap=gt_overlap, 
+            hamiltonian=pred_ham, 
+            transform=False
+            )
+        pred_energy = calc_mf.energy_tot(pred_density)
+        pred_data["calc_energy"] = pred_energy
+        
+        gt_density, gt_res = calc_dm0_from_ham(
+            atoms=atoms, 
+            overlap=gt_overlap, 
+            hamiltonian=gt_ham, 
+            transform=False
+            )
+        gt_energy = calc_mf.energy_tot(gt_density)
+        gt_data["calc_energy"] = gt_energy
+        
+        pred_mo_energy = pred_res["orbital_energies"].squeeze().numpy()
+        pred_mo_coeff = pred_res["orbital_coefficients"].squeeze().numpy()
+        pred_data["calc_mo_energy"] = pred_mo_energy
+        pred_data["calc_mo_coeff"] = pred_mo_coeff
+
+        gt_mo_energy = gt_res["orbital_energies"].squeeze().numpy()
+        gt_mo_coeff = gt_res["orbital_coefficients"].squeeze().numpy()
+        gt_data["calc_mo_energy"] = gt_mo_energy
+        gt_data["calc_mo_coeff"] = gt_mo_coeff
+        
+        pred_mo_occ = calc_mf.get_occ(pred_mo_energy, pred_mo_coeff)
+        pred_data["mo_occ"] = pred_mo_occ
+        pred_forces = -grad_frame.kernel(mo_energy=pred_mo_energy, mo_coeff=-pred_mo_coeff, mo_occ=pred_mo_occ)
+        pred_data["calc_forces"] = pred_forces
+
+        gt_mo_occ = calc_mf.get_occ(gt_mo_energy, gt_mo_coeff)
+        gt_data["mo_occ"] = gt_mo_occ
+        gt_forces = -grad_frame.kernel(mo_energy=gt_mo_energy, mo_coeff=-gt_mo_coeff, mo_occ=gt_mo_occ)
+        gt_data["calc_forces"] = gt_forces
+
+        if not debug:
             torch.save(pred_data, pred_file_path)
-        if "calc_force" in gt_data and not NEW_CALC:
-            gt_energy = gt_data["calc_energy"]
-            gt_forces = gt_data["calc_forces"]
-            gt_mo_energy = gt_data["calc_mo_energy"]
-            gt_mo_coeff = gt_data["calc_mo_coeff"]
-        else:
-            gt_hamiltonian = gt_data["hamiltonian"] + remove_init * gt_data["init_ham"].squeeze(0)
-            calc_overlap = calc_data["overlap"].unsqueeze(0) # (gt_overlap - calc_overlap) has float32 precision error (1e^-7)
-            gt_ham = matrix_transform_single(gt_hamiltonian.unsqueeze(0), atoms, convention="back2pyscf")
-            
-            gt_density, gt_res = calc_dm0_from_ham(atoms, calc_overlap, gt_ham, transform=False)
-            gt_energy = calc_mf.energy_tot(gt_density)
-            gt_data["calc_energy"] = gt_energy
-
-            gt_mo_energy = gt_res["orbital_energies"].squeeze().numpy()
-            gt_mo_coeff = gt_res["orbital_coefficients"].squeeze().numpy()
-            gt_data["calc_mo_energy"] = gt_mo_energy
-            gt_data["calc_mo_coeff"] = gt_mo_coeff
-
-            mo_occ = calc_mf.get_occ(gt_mo_energy, gt_mo_coeff)
-            gt_data["mo_occ"] = mo_occ
-            gt_forces = -grad_frame.kernel(mo_energy=gt_mo_energy, mo_coeff=-gt_mo_coeff, mo_occ=mo_occ)
-            gt_data["calc_forces"] = gt_forces
-            
-            # save gt_data
             torch.save(gt_data, gt_file_path)
 
-        pred_forces_norm = np.linalg.norm(pred_forces, axis=1)
-        calc_forces_norm = np.linalg.norm(calc_forces, axis=1)
-        gt_forces_norm = np.linalg.norm(gt_forces, axis=1)
+    pred_forces_norm = np.linalg.norm(pred_forces, axis=1)
+    calc_forces_norm = np.linalg.norm(calc_forces, axis=1)
+    gt_forces_norm = np.linalg.norm(gt_forces, axis=1)
 
-        num_occ = int(gt_data["atoms"].sum() / 2)
+    num_occ = int(gt_data["atoms"].sum() / 2)
 
-        # Extract occupied orbital energies only
-        pred_mo_energy_occ = pred_mo_energy[:num_occ]
-        gt_mo_energy_occ = gt_mo_energy[:num_occ]
-        calc_mo_energy_occ = calc_mo_energy[:num_occ]
+    # Extract occupied orbital energies only
+    pred_mo_energy_occ = pred_mo_energy[:num_occ]
+    gt_mo_energy_occ = gt_mo_energy[:num_occ]
+    calc_mo_energy_occ = calc_mo_energy[:num_occ]
 
 
-        result = {
-            "data_index": data_index,
+    result = {
+        "data_index": data_index,
 
-            "hamiltonian_diff (pred-gt)": abs(pred_ham - gt_ham).mean(),
-            "hamiltonian_diff (pred-calc)": abs(pred_ham - calc_ham).mean(),
-            "hamiltonian_diff (gt-calc)": abs(gt_ham - calc_ham).mean(),
+        "hamiltonian_diff (pred-gt)": abs(pred_ham - gt_ham).mean(),
+        "hamiltonian_diff (pred-calc)": abs(pred_ham - calc_ham).mean(),
+        "hamiltonian_diff (gt-calc)": abs(gt_ham - calc_ham).mean(),
 
-            "pred_energy": pred_energy,
-            "gt_energy": gt_energy,
-            "calc_energy": calc_energy,
-            
-            "energy_diff (pred-gt)": abs(pred_energy - gt_energy),
-            "energy_diff (pred-calc_energy)": abs(pred_energy - calc_energy),
-            "energy_diff (gt-calc_energy)": abs(gt_energy - calc_energy),
+        "pred_energy": pred_energy,
+        "gt_energy": gt_energy,
+        "calc_energy": calc_energy,
+        
+        "energy_diff (pred-gt)": abs(pred_energy - gt_energy),
+        "energy_diff (pred-calc_energy)": abs(pred_energy - calc_energy),
+        "energy_diff (gt-calc_energy)": abs(gt_energy - calc_energy),
 
-            "pred_force": pred_forces,
-            "gt_force": gt_forces,
-            "calc_force": calc_forces,
+        "pred_force": pred_forces,
+        "gt_force": gt_forces,
+        "calc_force": calc_forces,
 
-            "forces_diff l2 (pred-gt)": abs(pred_forces - gt_forces).mean(),
-            "forces_diff l2 (pred-calc_forces)": abs(pred_forces - calc_forces).mean(),
-            "forces_diff l2 (gt-calc_forces)": abs(gt_forces - calc_forces).mean(),
+        "forces_diff l2 (pred-gt)": abs(pred_forces - gt_forces).mean(),
+        "forces_diff l2 (pred-calc_forces)": abs(pred_forces - calc_forces).mean(),
+        "forces_diff l2 (gt-calc_forces)": abs(gt_forces - calc_forces).mean(),
 
-            "pred_force_norm": pred_forces_norm,
-            "gt_force_norm": gt_forces_norm,
-            "calc_force_norm": calc_forces_norm,
+        "pred_force_norm": pred_forces_norm,
+        "gt_force_norm": gt_forces_norm,
+        "calc_force_norm": calc_forces_norm,
 
-            "pred_force_norm_diff (pred-gt)": abs(pred_forces_norm - gt_forces_norm).mean(),
-            "pred_force_norm_diff (pred-calc_forces)": abs(pred_forces_norm - calc_forces_norm).mean(),
-            "gt_force_norm_diff (gt-calc_forces)": abs(gt_forces_norm - calc_forces_norm).mean(),
+        "pred_force_norm_diff (pred-gt)": abs(pred_forces_norm - gt_forces_norm).mean(),
+        "pred_force_norm_diff (pred-calc_forces)": abs(pred_forces_norm - calc_forces_norm).mean(),
+        "gt_force_norm_diff (gt-calc_forces)": abs(gt_forces_norm - calc_forces_norm).mean(),
 
-            "orbital_coeff_similarity (pred-gt)": torch.cosine_similarity(torch.tensor(pred_mo_coeff), torch.tensor(gt_mo_coeff), dim=1).abs().mean(),
-            "orbital_coeff_similarity (pred-calc)": torch.cosine_similarity(torch.tensor(pred_mo_coeff), torch.tensor(calc_mo_coeff), dim=1).abs().mean(),
-            "orbital_coeff_similarity (gt-calc)": torch.cosine_similarity(torch.tensor(gt_mo_coeff), torch.tensor(calc_mo_coeff), dim=1).abs().mean(),
+        "orbital_coeff_similarity (pred-gt)": torch.cosine_similarity(torch.tensor(pred_mo_coeff), torch.tensor(gt_mo_coeff), dim=1).abs().mean(),
+        "orbital_coeff_similarity (pred-calc)": torch.cosine_similarity(torch.tensor(pred_mo_coeff), torch.tensor(calc_mo_coeff), dim=1).abs().mean(),
+        "orbital_coeff_similarity (gt-calc)": torch.cosine_similarity(torch.tensor(gt_mo_coeff), torch.tensor(calc_mo_coeff), dim=1).abs().mean(),
 
-            "occupied_orbital_energy_mae (pred-gt)": np.abs(pred_mo_energy_occ - gt_mo_energy_occ).mean(),
-            "occupied_orbital_energy_mae (pred-calc)": np.abs(pred_mo_energy_occ - calc_mo_energy_occ).mean(),
-            "occupied_orbital_energy_mae (gt-calc)": np.abs(gt_mo_energy_occ - calc_mo_energy_occ).mean(),
-        }
+        "occupied_orbital_energy_mae (pred-gt)": np.abs(pred_mo_energy_occ - gt_mo_energy_occ).mean(),
+        "occupied_orbital_energy_mae (pred-calc)": np.abs(pred_mo_energy_occ - calc_mo_energy_occ).mean(),
+        "occupied_orbital_energy_mae (gt-calc)": np.abs(gt_mo_energy_occ - calc_mo_energy_occ).mean(),
+
+        "overlap_diff (gt-calc)": np.abs(gt_overlap - calc_overlap).mean(),
+    }
+    # unit of pyscf calculation
+    # distance in bohr
+    # energy in hartree
+    # forces in hartree/bohr
+    # hamiltonian in hartree
+    """
     except Exception as e:
         print(f"Error processing molecule {data_index}: {str(e)}")
         result = {
             "error_count": data_index,
         }
+    """
     return result
 
 
@@ -208,6 +229,7 @@ if __name__ == "__main__":
     parser.add_argument("--gt_prefix", type=str, default="gt_")
     parser.add_argument("--num_procs", type=int, default=1)
     parser.add_argument("--debug", default=False, action="store_true")
+    parser.add_argument("--size_limit", type=int, default=-1)
     args = parser.parse_args()
 
     dir_path = args.dir_path
@@ -227,7 +249,9 @@ if __name__ == "__main__":
     # Create list of (pred_path, gt_path) tuples
     file_pairs = list(zip(list_pred_paths, list_gt_paths))
     if args.debug:
-        file_pairs = file_pairs[:1]
+        args.size_limit = 1
+    if args.size_limit > 0:
+        file_pairs = file_pairs[:args.size_limit]
 
     print(f"Processing {len(file_pairs)} molecules with {num_procs} processes...")
 
